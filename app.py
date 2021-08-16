b84695093b6b5537f8a02a90a611539e482c22a6
Jacob de Vries <mrjacobdevries@gmail.com>
2021-08-16 15:25:59 -0700

    Working on mobile first programming. Setting up site functionality.

--- app.py
+++ app.py
@@ -5,28 +5,42 @@ from flask_cors import CORS
 from flask_pymongo import PyMongo
 import pymongo
 # Endpoints
+import json
 from bson import json_util
 from bson.json_util import dumps
-# Access
-from config import mongoPW
 
 # App
 app = Flask(__name__)
 CORS(app)
 app.config['DEBUG'] = True
-mongoURI = f'mongodb+srv://dbUser:{mongoPW}@cluster0.cscep.mongodb.net/test'
+
+# Local
+client = pymongo.MongoClient('mongodb+srv://dbUser:dbUserPassword@cluster0.cscep.mongodb.net/test')
+app.config['MONGO_URI'] = 'mongodb+srv://dbUser:dbUserPassword@cluster0.cscep.mongodb.net/test'
+# Heroku
+# import os
+# client = pymongo.MongoClient(os.environ['MONGO_URI'])
+# app.config['MONGO_URI'] = os.environ['MONGO_URI']
 
 # Connection to MongoDB Atlas -- Local App
-app.config['MONGO_URI'] = mongoURI
-connection = PyMongo(app)
-collection = connection.db['BookClub']
+mongo = PyMongo(app)
+db = mongo.db['BookClub']
+books = db['books']
 
 # Routes
 # Home Route
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+@app.route('/books/')
+def futureBooks():
+    return render_template('books.html')
 
+# Data Routes
+@app.route('/books/data/')
+def data():
+    bookslist = list(books.find())
+    return json.dumps(books, default=json_util.default)
 # Debug
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/zwaz.js
@@ -0,0 +1,28 @@
+// Toggle Mobile Nav Menu
+let mobileNav = d3.select('#mobileNavBar');
+let navMenu = d3.select('#mobileNavMenu');
+mobileNav.on('click', function() {
+    let navMenuC = navMenu.attr('class')
+    if (navMenuC.includes('noDisplay')) {
+        navMenu = navMenu.classed('noDisplay', false);
+    } else {
+        navMenu = navMenu.classed('noDisplay', true);
+    };
+});
+
+// Mobile Menu Navigation
+let mobilePages = d3.selectAll('.navMenuItem');
+mobilePages.on('click', function() {
+    let page = d3.select(this)._groups[0][0].attributes[1].value;
+    if (page !== 'home') {
+        location = `/${page}/`;
+    } else {
+        location = '/';
+    }
+});
+
+// Title Link Navigation
+let titleLink = d3.select('#nameHeader');
+titleLink.on('click', function() {
+    location = '/';
+});
