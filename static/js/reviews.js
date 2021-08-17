e43bdc25dd763af73e65e2df1f57f186d59623e4
Jacob de Vries <mrjacobdevries@gmail.com>
2021-08-17 15:30:06 -0700

    Base done. Need to add ability to input reviews.

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -12,5 +12,13 @@ function reviewSearch() {
     console.log(personSearch)
     d3.json(`${linkFirstPart}/reviews/${titleSearch}/${personSearch}/`).then(reviews => {
         console.log(reviews)
+        let reviewDisplay = d3.select('#reviewDisplay').html('');
+        reviews.forEach(review => {
+            nameCompressed = ((review.name).replaceAll(' ','')).replaceAll('.','');
+            titleCompressed = ((review.title).replaceAll(' ','')).replaceAll('.','');
+            let reviewBox = reviewDisplay.append('div').classed('bookReview', true).attr('id',`${nameCompressed}${titleCompressed}`);
+            reviewBox.append('h1').classed('reviewHeader', true).text(`${review.name} - ${review.title} - ${review.rating}`);
+            reviewBox.append('p').classed('reviewContent', true).text(review.content);
+        });
     });
 };
